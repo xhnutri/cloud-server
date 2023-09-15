4bde9ec8ed1816f654434ef041e93fed4f804221
Miguel Angel Hernandez Martinez <32290985+xhnutri@users.noreply.github.com>
2023-09-16 00:02:20 +0200

    Update gamepad.py

--- gamepad.py
+++ gamepad.py
@@ -382,4 +382,4 @@ async def disconnect(sid):
 
 if __name__ == "__main__":
     print("Init Server")
-    uvicorn.run(app, host="0.0.0.0", port=8090)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
