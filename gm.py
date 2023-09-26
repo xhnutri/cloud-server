01a260b02a2b47e3ae52c5407e8b041e4f463c07
Miguel Angel Hernandez Martinez <32290985+xhnutri@users.noreply.github.com>
2023-09-26 02:57:34 +0200

    Update gm.py

--- gm.py
+++ gm.py
@@ -6,7 +6,7 @@ from pynput.mouse import Button, Controller
 from pynput.keyboard import Key
 # from pynput  
 
-keyboard = pynput.kerboard.Controller();
+keyboard = pynput.keyboard.Controller();
 
 # Press and release space
 keyboard.press(Key.space)
